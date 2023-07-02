057638ac9484bc182c14eba5df091061dc00db74
BigDaddyNFT <136732467+BigDaddyNFT@users.noreply.github.com>
2023-07-02 13:15:54 +0300

    Update BigDaddy-flow-config.js

--- BigDaddyNFT/BigDaddy-flow-config.js
+++ BigDaddyNFT/BigDaddy-flow-config.js
@@ -3,7 +3,9 @@ import { config } from "@onflow/fcl";
 config({
     "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
     "accessNode.api": "https://rest-testnet.onflow.org",
-    "flow.network": "testnet"
+    "flow.network": "testnet",
+    "app.detail.icon": "https://icons8.com/icon/RAz1RJl6omnd/instructure",
+    "app.detail.title": "BigDaddy NFT"
     //,  Mainnet: "https://rest-mainnet.onflow.org"
   //"discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn" // Mainnet: "https://fcl-discovery.onflow.org/authn"
 })
